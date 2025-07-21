7db51f1198776e47f415cc7b5f0153fedd8e555d
Willian Ferreira <willianhf15@gmail.com>
2025-07-21 16:30:55 +0100

    install: add pnpm and vscode

--- install.sh
+++ install.sh
@@ -14,11 +14,11 @@ apps=(
   stow
   neovim
   zoxide
-  spaceship
   oven-sh/bun/bun
   fzf
   orbstack
   fnm
+  pnpm
 )
 
 brew install "${apps[@]}"
@@ -30,6 +30,12 @@ brew install --cask 1password
 brew install --cask thebrowsercompany-dia
 brew install --cask discord
 brew install --cask whatsapp
+brew install --cask visual-studio-code
 
 # installs xcode cli tools
 xcode-select --install
+#
+# installs spaceship theme
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
