ac1e3014aef02fa00c5f0b914397e4f027d5c31d
Willian Ferreira <willianhf15@gmail.com>
2025-07-21 15:11:29 +0100

    install: add 1password and dia browser

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 # installs homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# installs oh-my-zsh
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 brew update
 brew upgrade
 
@@ -23,9 +26,8 @@ brew install "${apps[@]}"
 brew install --cask raycast
 brew install --cask ghostty
 brew install --cask spotify
-
-# installs oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+brew install --cask 1password
+brew install --cask thebrowsercompany-dia
 
 # installs xcode cli tools
 xcode-select --install
